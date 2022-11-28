95908fb0ea42cf3aafa9aa9a43e86f1c8d356309
zrr <85417492+zreu-zrr@users.noreply.github.com>
2022-11-28 13:03:33 -0800

    Add files via upload

---
+++ linux_cyber/delete.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+sudo find /home -name "*.mp3" -delete
+sudo find /home -name "*.mp4" -delete
---
+++ linux_cyber/edit.sh
@@ -0,0 +1,4 @@
+
+sudo cp scripts/sshd_config /etc/ssh/sshd_config
+sudo cp scripts/common-password /etc/pam.d/common-password
+sudo cp scripts/passwd /etc/pam.d/passwd
