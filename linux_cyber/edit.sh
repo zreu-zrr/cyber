cd5ce85ee9830d56f0a5fa4ae4948ba617665f1b
zrr <85417492+zreu-zrr@users.noreply.github.com>
2022-11-28 14:08:09 -0700

    Update edit.sh

--- linux_cyber/edit.sh
+++ linux_cyber/edit.sh
@@ -1,4 +1,4 @@
 
-sudo cp scripts/sshd_config /etc/ssh/sshd_config
-sudo cp scripts/common-password /etc/pam.d/common-password
-sudo cp scripts/passwd /etc/pam.d/passwd
+sudo cp ./files/sshd_config /etc/ssh/sshd_config
+sudo cp ./files/common-password /etc/pam.d/common-password
+sudo cp ./files/passwd /etc/pam.d/passwd
