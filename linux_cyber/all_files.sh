0ac69857fccab135ff2b4b15a5f84addf9483fcb
zrr <85417492+zreu-zrr@users.noreply.github.com>
2022-12-07 15:31:07 -0700

    Update all_files.sh

--- linux_cyber/all_files.sh
+++ linux_cyber/all_files.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo cp files/login.defs /etc/login.defs
-sudo cp files/common-auth /etc/pam.d/common-auth
+#sudo cp files/common-auth /etc/pam.d/common-auth
 sudo cp files/sudoers /etc/sudoers
 
 sudo cp ./files/sshd_config /etc/ssh/sshd_config
